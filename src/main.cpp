4c496bd4c91ed9c142ff35ed9df3b742d9ea28e1
Greg Liebig <greg@engrinnovations.com>
2022-01-27 20:56:56 -0600

    Initial COmmit

---
+++ src/main.cpp
@@ -0,0 +1,86 @@
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+
+// Code to read the output from Light O Rama S5 HS UART
+// controller (RED) to develop a
+// Debugging Device to use in the field to check
+// signal transmission and hardware issues
+// Greg Liebig 01/27/31 Initial commit and a work in progress
+
+/*
+Aruduino Mega 2560 Outputting Serial to Monitor using Serial
+and using Serial1 for input from
+DSD TECH SH-U12 RS485 to TTL 5V Board with MAX13487 Chip (Serial1) or
+RS485 chip provided by LOR and uxcell a15080400ux1059 Solder Type Double Row 8PIN DIP Integrated Circuit IC Socket (Serial2) or
+Max485 Chip RS-485 Module TTL to RS-485 Module with 120ohm RZ terminating resistor removed (Serial3)
+
+with the hope to eventually write meaningful data to
+ELEGOO UNO R3 2.8 Inches TFT Touch Screen shield with SD Card Socket 
+
+*/
+
+unsigned int byteReceived1;
+unsigned int byteReceived3;
+
+ 
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial1.begin(57600); // Mega pins 18 (Tx) and 19 (Rx) for SH-U12
+  Serial3.begin(57600); // Mega pins 14 (Tx) and 15 (Rx) for Max485
+  Serial.println("RS485 LOR Read");
+  //pinMode(SSerialTxControl, OUTPUT);
+ 
+  // print Headers
+  Serial.print("Port"); 
+  Serial.print("\t");         
+  
+  
+  Serial.print("RAW");  
+  Serial.print("\t");         
+
+  Serial.print("DEC");
+  Serial.print("\t");
+
+  Serial.print("HEX");
+  Serial.print("\t");
+
+  Serial.print("OCT");
+  Serial.print("\t");
+
+  Serial.print("BIN");
+  Serial.println();       
+}
+ 
+void loop() {
+
+  // now we read from the RS485 bus
+  if (Serial1.available()) {
+    byteReceived1 = Serial1.read();
+    byteReceived3 = Serial3.read();
+ //.write(byteReceived );
+   Serial.print("Serial1");
+   Serial.print("\t");
+   Serial.write(byteReceived1);
+   Serial.print("\t");
+   Serial.print(byteReceived1, DEC);
+   Serial.print("\t");
+   Serial.print(byteReceived1, HEX);
+   Serial.print("\t");
+   Serial.print(byteReceived1, OCT);
+   Serial.print("\t");
+   Serial.println(byteReceived1, BIN);
+  
+   Serial.print("Serial3");
+   Serial.print("\t");
+   Serial.write(byteReceived3);
+   Serial.print("\t");
+   Serial.print(byteReceived3, DEC);
+   Serial.print("\t");
+   Serial.print(byteReceived3, HEX);
+   Serial.print("\t");
+   Serial.print(byteReceived3, OCT);
+   Serial.print("\t");
+   Serial.println(byteReceived3, BIN);
+  }
+}
