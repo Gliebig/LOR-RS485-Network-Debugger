2eddb3656ada0fa6e96fcf01d6b2ca898de309bc
Gliebig <35844174+Gliebig@users.noreply.github.com>
2022-02-02 16:08:05 -0600

    Working for non motion FX

--- src/main.cpp
+++ src/main.cpp
@@ -17,79 +17,67 @@ Max485 Chip RS-485 Module TTL to RS-485 Module with 120ohm RZ terminating resist
 with the hope to eventually write meaningful data to
 ELEGOO UNO R3 2.8 Inches TFT Touch Screen shield with SD Card Socket 
 
+Initial code to transpose hex values received from Serial1 into "command lines" 1/29/22
+running into a problem since the heartbeat is constanly being transmitted with the pattern
+0x00, 0xFF, 0X81, 0x56, 0x00
+but the control data has an extra 0x00 at the end eg:
+0x00, 0xA, 0x3, 0x1, 0x88, 0x00, 0x00
+Start 0
+UID
+Action
+Channel
+Working for sequence output without motion FX 2/2/22. Buggy when it comes to caputuring sequence output with motion FX
+
 */
 
 unsigned long byteReceived1;
-unsigned long byteReceived3;
-unsigned long myTime;
+unsigned long myTime = 0;
+#define Start = 0
+unsigned long PacketEnd = 0;
+unsigned long PacketStart = 0;
 
  
 void setup() {
-  // put your setup code here, to run once:
-
   Serial.begin(57600);
   Serial1.begin(57600); // Mega pins 18 (Tx) and 19 (Rx) for SH-U12
-  Serial3.begin(57600); // Mega pins 14 (Tx) and 15 (Rx) for Max485
+  //Serial3.begin(57600); // Mega pins 14 (Tx) and 15 (Rx) for Max485
   Serial.println("RS485 LOR Read");
-  //pinMode(SSerialTxControl, OUTPUT);
- 
-  // print Headers
+   // print Headers
   Serial.print("Time"); 
   Serial.print("\t");         
-  
-  
   Serial.print("Serial1");  
   Serial.print("\t");         
-
-  Serial.print("Serial3");
-  Serial.print("\t");
-
-  //Serial.print("HEX");
-  //Serial.print("\t");
-
-  //Serial.print("OCT");
-  //Serial.print("\t");
-
-  //Serial.print("BIN");
   Serial.println();       
 }
  
 void loop() {
-  myTime = millis();
+//  myTime = millis();
+//  Serial.print(myTime);
+//  Serial.print("\t");
   // now we read from the RS485 bus
-  if (Serial1.available()) {
-    byteReceived1 = Serial1.read();
- //.write(byteReceived );
-   Serial.print(myTime);
-   Serial.print("\t");
-   //Serial.write(byteReceived1);
-   //Serial.print("\t");
-   //Serial.print(byteReceived1, DEC);
-   //Serial.print("\t");
-   Serial.print(byteReceived1, HEX);
-   Serial.print("\t");
-   //Serial.print(byteReceived1, OCT);
-   //Serial.print("\t");
-   //Serial.println(byteReceived1, BIN);
-  }
-  if (Serial3.available()) {
-   byteReceived3 = Serial3.read();
-   Serial.print(byteReceived3, HEX);
-   Serial.print("\t");
-   /*
-   // Using the 485 Max Board
-   Serial.print("Serial3");
-   Serial.print("\t");
-   Serial.write(byteReceived3);
-   Serial.print("\t");
-   Serial.print(byteReceived3, DEC);
-   Serial.print("\t");
-   Serial.print(byteReceived3, HEX);
-   Serial.print("\t");
-   Serial.print(byteReceived3, OCT);
-   Serial.print("\t");
-   Serial.println(byteReceived3, BIN);
-   */
-  }
-  Serial.println();
+    if (Serial1.available()) {
+      byteReceived1 = Serial1.read();
+    
+       if ((byteReceived1 == 0x00) & (Serial1.peek() != 0x00)) {
+          if (PacketStart==1) {
+          Serial.print("0x00");
+          Serial.println(); 
+          }
+          PacketStart = 0;
+          PacketEnd = 1;
+        }
+      
+        else {
+          //Serial.print("Packet Start");
+          //Serial.print("\t");
+          //Serial.print(byteReceived1, HEX);
+          //Serial.print("\t");
+          PacketEnd = 0;
+          PacketStart = 1;
+        }
+      if (PacketStart == 1) {
+         Serial.print(byteReceived1, HEX);
+         Serial.print("\t");
+      }
+    }    
 }
